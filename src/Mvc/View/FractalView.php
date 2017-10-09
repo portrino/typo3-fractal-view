804958c4d6c7be2e451146a704315a01c085cabf
aWuttig <andr.wuttig@gmail.com>
2017-10-09 07:30:46 +0200

    * [CLEANUP] Fix PSR2 error

--- src/Mvc/View/FractalView.php
+++ src/Mvc/View/FractalView.php
@@ -137,7 +137,7 @@ class FractalView extends JsonView
             $this->setExcludes($this->controllerContext->getRequest()->getArgument('_excludes'));
         }
     }
-    
+
     /**
      * Loads the configuration and transforms the value to a serializable
      * array via fractal
@@ -187,9 +187,10 @@ class FractalView extends JsonView
         $result = $value;
         if (is_array($value) || $value instanceof ArrayAccess) {
             $result = $this->transformCollection($value, $configuration);
-        }
-        else if (is_object($value)) {
-            $result = $this->transformObject($value, $configuration);
+        } else {
+            if (is_object($value)) {
+                $result = $this->transformObject($value, $configuration);
+            }
         }
         return $result;
     }
